6c3ddee1c9457bca9b4d1aadfc0f4acbdea423c2
Angelo <alozanome@isise.edu.pe>
2024-12-09 21:22:21 -0500

    examenparcial

---
+++ Controllers/EquipoController.cs
@@ -0,0 +1,139 @@
+﻿using DocumentFormat.OpenXml.Spreadsheet;
+using ExamenParcialLozano.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ExamenParcialLozano.Controllers
+{
+    public class EquipoController : Controller
+    {
+        // GET: Equipo
+        public ActionResult Index()
+        {
+            using (JugadoresEntity contexto = new JugadoresEntity())
+            {
+                return View(contexto.Equipos.ToList());
+            }
+        }
+
+        // GET: Equipo/Details/5
+        public ActionResult Details(int id)
+        {
+            using (JugadoresEntity contexto = new JugadoresEntity())
+            {
+                return View(contexto.Equipos.Where(x => x.ID_Equipo == id).FirstOrDefault());
+            }
+                
+        }
+
+        // GET: Equipo/Create
+        public ActionResult Create()
+        {
+            using (JugadoresEntity contexto = new JugadoresEntity())
+            {
+                return View();
+            }
+        }
+
+        // POST: Equipo/Create
+        [HttpPost]
+        public ActionResult Create(Equipos EquiposArray)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+                using (JugadoresEntity contexto = new JugadoresEntity())
+                {
+                    contexto.Equipos.Add(EquiposArray);
+                    contexto.SaveChanges();
+                }
+                return RedirectToAction("/Create");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Equipo/Edit/5
+        public ActionResult Edit(int id)
+        {
+            using (JugadoresEntity contexto = new JugadoresEntity())
+            {
+                Equipos equipo = contexto.Equipos.Find(id); // Busca el equipo por ID
+
+                if (equipo == null)
+                {
+                    return HttpNotFound(); // Devuelve un 404 si no se encuentra el equipo
+                }
+
+                return View(equipo);
+            }
+        }
+
+        // POST: Equipo/Edit/5
+        [HttpPost]
+        public ActionResult Edit(Equipos equipo) // Recibe el objeto Equipos directamente
+        {
+            try
+            {
+                using (JugadoresEntity contexto = new JugadoresEntity())
+                {
+                    // Adjunta el equipo al contexto si no se está rastreando
+                    contexto.Equipos.Attach(equipo);
+
+                    // Marca el equipo como modificado
+                    var entry = contexto.Entry(equipo);
+                    entry.State = EntityState.Modified;
+
+                    contexto.SaveChanges();
+                }
+                return RedirectToAction("Index"); // Redirige a la acción Index del mismo controlador
+            }
+            catch (Exception ex)
+            {
+                // Registra la excepción para depuración
+                // Logger.Error(ex); // Reemplaza Logger con tu mecanismo de registro
+
+                ModelState.AddModelError("", "Error al actualizar el equipo. Por favor, inténtalo de nuevo."); // Agrega un mensaje de error al ModelState
+                return View(equipo); // Vuelve a mostrar la vista con el mensaje de error
+            }
+        }
+
+        // GET: Equipo/Delete/5
+        public ActionResult Delete(int id)
+        {
+            using (JugadoresEntity contexto = new JugadoresEntity())
+            {
+                
+                return View(contexto.Equipos.Where(x => x.ID_Equipo == id).FirstOrDefault());
+            }
+ 
+        }
+
+        // POST: Equipo/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+                using (JugadoresEntity contexto = new JugadoresEntity())
+                {
+                    Equipos objEquipos = contexto.Equipos.Where(x => x.ID_Equipo == id).FirstOrDefault();
+                    contexto.Equipos.Remove(objEquipos);
+                    contexto.SaveChanges();
+                }
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Controllers/JugadorController.cs
@@ -0,0 +1,123 @@
+﻿using ExamenParcialLozano.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ExamenParcialLozano.Models;
+using System.Data;
+
+namespace ExamenParcialLozano.Controllers
+{
+    public class JugadorController : Controller
+    {
+        // GET: Jugador
+        public ActionResult Index()
+        {
+            using (JugadoresEntity contexto=new JugadoresEntity())
+            {
+                return View(contexto.Jugadores.ToList());
+            }
+        }
+
+        // GET: Jugador/Details/5
+        public ActionResult Details(int id)
+        {
+            using (JugadoresEntity contexto = new JugadoresEntity())
+            {
+                return View(contexto.Jugadores.Where(x=> x.ID_Jugador==id).FirstOrDefault());
+            }
+                
+        }
+
+        // GET: Jugador/Create
+        public ActionResult Create()
+        {
+            using (JugadoresEntity contexto = new JugadoresEntity())
+            {
+                return View();
+            }
+        }
+
+        // POST: Jugador/Create
+        [HttpPost]
+        public ActionResult Create(Jugadores jugadoresArray)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+                using (JugadoresEntity contexto = new JugadoresEntity())
+                {
+                    contexto.Jugadores.Add(jugadoresArray);
+                    contexto.SaveChanges();
+                }
+                return RedirectToAction("/Create");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Jugador/Edit/5
+        public ActionResult Edit(int id)
+        {
+            using (JugadoresEntity contexto = new JugadoresEntity())
+            {
+                return View(contexto.Jugadores.Where(x => x.ID_Jugador == id).FirstOrDefault());
+            }
+        }
+
+        // POST: Jugador/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, Jugadores jugadoresArray)
+        {
+            try
+            {
+                // TODO: Add update logic here
+                using (JugadoresEntity contexto = new JugadoresEntity())
+                {
+                    contexto.Entry(jugadoresArray).State=EntityState.Modified;
+                    contexto.SaveChanges();
+                }
+                    return RedirectToAction("/");
+            }
+            catch
+            {
+                return View("/");
+            }
+        }
+
+        // GET: Jugador/Delete/5
+        public ActionResult Delete(int id)
+        {
+            using (JugadoresEntity contexto = new JugadoresEntity())
+            {
+                
+                return View(contexto.Jugadores.Where(x => x.ID_Jugador == id).FirstOrDefault());
+            }
+               
+        }
+
+        // POST: Jugador/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+                using (JugadoresEntity contexto = new JugadoresEntity())
+                {
+                    Jugadores objJugadores = contexto.Jugadores.Where(x => x.ID_Jugador == id).FirstOrDefault();
+                    contexto.Jugadores.Remove(objJugadores);
+                    contexto.SaveChanges();
+                }
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
